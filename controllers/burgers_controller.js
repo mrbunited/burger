eb221584212a3df84de12191289b81d3b4e13344
mradke2 <mrbunited@gmail.com>
2018-09-04 18:41:39 -0500

    create functional

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 
-  router.post("/", function(req, res) {
+  router.post("/api/burgers", function(req, res) {
     burger.create([
       "burger_name", "devoured"
     ], [
