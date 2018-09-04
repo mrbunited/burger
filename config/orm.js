8aeb77c3964b8af52293569973f9011f59f53344
mradke2 <mrbunited@gmail.com>
2018-09-03 22:49:45 -0500

    handlebars

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+var connection = require("./connection.js");
+
+var orm = {
+  select: function(whatToSelect, tableInput) {
+    var queryString = "SELECT ?? FROM ??";
+    connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+
+};
+  module.exports = orm;
--- server.js
+++ server.js
@@ -1,5 +1,29 @@
-var orm = require("./config/orm.js");
-
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
